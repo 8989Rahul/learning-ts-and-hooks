adf386eea484174ccc4fa2e2c63d0d65f9a85470
8989Rahul <daghalerahul0@gmail.com>
2019-10-16 15:16:31 +0530

    learn't useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,63 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useCallback } from "react";
 import "./App.css";
-import { useForm } from "./useForm";
 import { ShowHello } from "./ShowHello";
-// import { useFetch } from "./useFetch";
 
 const App: React.FC = () => {
-  const [values, setValues] = useForm({ email: "", password: "" });
-  const inputRef = useRef(null);
-  const hello = useRef(() => console.log("hello"));
-  const [showHello, setShowHello] = useState(true);
+  let [count, setCount] = useState(0);
+
+  const increment = useCallback(() => {
+    setCount(count++);
+  }, []);
 
   return (
     <div className="App">
-      <input
-        ref={inputRef.current}
-        name="email"
-        value={values.email}
-        onChange={setValues}
-      />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={setValues}
-      />
-
-      <button onClick={() => setShowHello(!showHello)}>Toggle</button>
-      <button
-        onClick={() => {
-          hello.current();
-        }}
-      >
-        Focus
-      </button>
-      {showHello && <ShowHello />}
+      <ShowHello increment={increment} />
+      <div>{count}</div>
     </div>
   );
 };
 
 export default App;
+
+// import React, { useState, useRef } from "react";
+// import "./App.css";
+// import { useForm } from "./useForm";
+// import { ShowHello } from "./ShowHello";
+// // import { useFetch } from "./useFetch";
+
+// const App: React.FC = () => {
+//   const [values, setValues] = useForm({ email: "", password: "" });
+//   const inputRef = useRef({} as any);
+//   const hello = useRef(() => console.log("hello"));
+//   const [showHello, setShowHello] = useState(true);
+
+//   return (
+//     <div className="App">
+//       <input
+//         ref={node => (inputRef.current = node)}
+//         name="email"
+//         value={values.email}
+//         onChange={setValues}
+//       />
+//       <input
+//         type="password"
+//         name="password"
+//         value={values.password}
+//         onChange={setValues}
+//       />
+
+//       <button onClick={() => setShowHello(!showHello)}>Toggle</button>
+//       <button
+//         onClick={() => {
+//           inputRef.current.focus();
+//           hello.current();
+//         }}
+//       >
+//         Focus
+//       </button>
+//       {showHello && <ShowHello />}
+//     </div>
+//   );
+// };
+
+// export default App;
