328957c7a3461d536e5fd0c72a1cd15ad8ec5881
8989Rahul <daghalerahul0@gmail.com>
2019-10-16 15:18:36 +0530

    just rendering a console

--- src/useForm.tsx
+++ src/useForm.tsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import { useRef } from "react";
 
-export const useForm = (initialValues: any) => {
-  const [values, setValues] = useState(initialValues);
-  return [
-    values,
-    (e: any) => {
-      setValues({
-        ...values,
-        [e.target.name]: e.target.value
-      });
-    }
-  ];
+export const useForm = () => {
+  const render = useRef(0);
+  console.log("render", render.current++);
 };
+
+// import { useState } from "react";
+
+// export const useForm = (initialValues: any) => {
+//   const [values, setValues] = useState(initialValues);
+//   return [
+//     values,
+//     (e: any) => {
+//       setValues({
+//         ...values,
+//         [e.target.name]: e.target.value
+//       });
+//     }
+//   ];
+// };
